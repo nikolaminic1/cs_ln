2829b184d8213440532e8be8922d5a6641f22b55
nikolaminic1 <49018678+nikolaminic1@users.noreply.github.com>
2022-03-15 01:25:24 +0100

    asdas

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.IO;
+using System.Xml;
+using static System.Convert;
+using static System.Console;
+using static ConsoleApp1.CustomApp;
+
+
+namespace ConsoleApp1
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            double m = 3.83214;
+            int k = (int)m;
+            int l = ToInt32(m);
+            WriteLine(l);
+            double lk = Math.Round(m);
+            WriteLine(lk);
+
+            DateTime now = DateTime.Now;
+            WriteLine(now);
+        }
+    }
+}
---
+++ WritingFunctions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConsoleApp1;
+
+namespace WritingFunctions
+{
+    class Program
+    {
+
+        static double Add(double b, double a)
+        {
+            return b * a;
+        }
+
+        static void Main(string[] args)
+        {
+            double a = 2.3;
+            double b = 2.5;
+            double c = Add(a, b);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
