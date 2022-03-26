5105a61569afd4a560bef49548baebe0edb9440a
nikolaminic1 <49018678+nikolaminic1@users.noreply.github.com>
2022-03-26 23:59:26 +0100

    Commit

---
+++ ConsoleApp2/ClassToTry/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp2.ClassToTry
+{
+    class Class1    
+    {
+        public static void Func()
+        {
+
+        }
+            
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using ConsoleApp2.ClassToTry;
 
 namespace ConsoleApp2
 {
@@ -7,6 +15,27 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
+
+            Trace.AutoFlush = true;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+            var ts = new TraceSwitch(
+                displayName: "PacktSwitch",
+                description: "Descrtiption"
+                );
+
+            configuration.GetSection("PacktSwitch").Bind(ts);
+
+            Trace.WriteLine(ts.TraceError, "Trace error");
+
+            Debug.WriteLine("Debug");
+            Trace.WriteLine("Trace");
         }
     }
 }
